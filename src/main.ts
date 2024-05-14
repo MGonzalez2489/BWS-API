842e89c6559c4a4449bd18ef23768b0345716850
ManuelGonzalez <manuel_agh@hotmail.com>
2024-05-14 17:40:07 -0600

    added env files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './_config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+  app.setGlobalPrefix('api');
+  await app.listen(envs.port);
+  logger.log(`SERVER STARTED ON PORT ${envs.port}`);
 }
 bootstrap();
